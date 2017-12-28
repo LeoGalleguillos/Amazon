637f1258412b85c0d16b35c7e5b62c13795ccf46
Leo Galleguillos <leo.galleguillos@icloud.com>
2017-12-28 07:52:42 -0800

    Create API service

---
+++ src/Model/Service/Api.php
@@ -0,0 +1,32 @@
+<?php
+namespace LeoGalleguillos\Amazon\Model\Service;
+
+use LeoGalleguillos\Amazon\Model\Table as AmazonTable;
+
+class Api
+{
+    public function __construct(AmazonTable\Api $amazonApiTable)
+    {
+        $this->amazonApiTable = $amazonApiTable;
+    }
+
+    /**
+     * Insert on duplicate key update.
+     */
+    public function insertOnDuplicateKeyUpdate($key, $value)
+    {
+        $this->amazonApiTable->insertOnDuplicateKeyUpdate($key, $value);
+    }
+
+    /**
+     * Was Amazon API called recently.
+     *
+     * @return bool
+     */
+    public function wasAmazonApiCalledRecently()
+    {
+        $lastCallMicrotime = $this->amazonApiTable->selectValueWhereKey('last_call_microtime');
+
+        return (microtime(true) - $lastCallMicrotime) < 1.5;
+    }
+}
---
+++ test/Model/Service/ApiTest.php
@@ -0,0 +1,60 @@
+<?php
+namespace LeoGalleguillos\AmazonTest\Model\Service;
+
+use LeoGalleguillos\Amazon\Model\Table as AmazonTable;
+use LeoGalleguillos\Amazon\Model\Service as AmazonService;
+use PHPUnit\Framework\TestCase;
+
+class ApiTest extends TestCase
+{
+    protected function setUp()
+    {
+        $this->apiTable = $this->createMock(AmazonTable\Api::class);
+        $this->apiService = new AmazonService\Api(
+            $this->apiTable
+        );
+    }
+
+    public function testInitialize()
+    {
+        $this->assertInstanceOf(AmazonService\Api::class, $this->apiService);
+    }
+
+    public function testInsertOnDuplicateKeyUpdate()
+    {
+        $this->assertNull($this->apiService->insertOnDuplicateKeyUpdate('foo', 'bar'));
+    }
+
+    public function testWasAmazonApiCalledRecently()
+    {
+        $this->apiTable->method('selectValueWhereKey')->will(
+            $this->onConsecutiveCalls(
+                null,
+                microtime(true),
+                microtime(true) - 1,
+                microtime(true) - 2,
+                9999999999,
+                1000000000
+            )
+        );
+
+        $this->AssertFalse(
+            $this->apiService->wasAmazonApiCalledRecently()
+        );
+        $this->AssertTrue(
+            $this->apiService->wasAmazonApiCalledRecently()
+        );
+        $this->AssertTrue(
+            $this->apiService->wasAmazonApiCalledRecently()
+        );
+        $this->AssertFalse(
+            $this->apiService->wasAmazonApiCalledRecently()
+        );
+        $this->AssertTrue(
+            $this->apiService->wasAmazonApiCalledRecently()
+        );
+        $this->AssertFalse(
+            $this->apiService->wasAmazonApiCalledRecently()
+        );
+    }
+}
