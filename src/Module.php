f96fb3f9a9ddffd486bc0312b91fdd05c89123d7
Leo Galleguillos <leo.galleguillos@icloud.com>
2017-12-28 08:19:37 -0800

    Add API service to service manager config

--- src/Module.php
+++ src/Module.php
@@ -1,6 +1,7 @@
 <?php
 namespace LeoGalleguillos\Amazon;
 
+use LeoGalleguillos\Amazon\Model\Service as AmazonService;
 use LeoGalleguillos\Amazon\Model\Table as AmazonTable;
 
 class Module
@@ -21,6 +22,11 @@ class Module
     {
         return [
             'factories' => [
+                AmazonService\Api::class => function ($serviceManager) {
+                    return new AmazonService\Api(
+                        $serviceManager->get(AmazonTable\Api::class)
+                    );
+                },
                 AmazonTable\Api::class => function ($serviceManager) {
                     return new AmazonTable\Api(
                         $serviceManager->get('amazon')
