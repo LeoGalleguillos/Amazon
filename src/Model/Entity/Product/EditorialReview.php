671ca664a75dd24dd9cf93471507b3d986c2603b
Leo Galleguillos <leo.galleguillos@icloud.com>
2018-01-08 12:48:02 -0800

    Add Editorial Review entity to repo

---
+++ src/Model/Entity/Product/EditorialReview.php
@@ -0,0 +1,15 @@
+<?php
+namespace LeoGalleguillos\Amazon\Model\Entity\Product;
+
+class EditorialReview
+{
+    /**
+     * @var string Content of the editorial review.
+     */
+    public $content;
+
+    /**
+     * @var string Source of the editorial review.
+     */
+    public $source;
+}
