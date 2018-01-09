cec54f5a99fbd568aee21269cfb88b34440d19e1
Leo Galleguillos <leo.galleguillos@icloud.com>
2018-01-09 14:39:06 -0800

    Create service to get root-relative URL

---
+++ src/Model/Service/Product/RootRelativeUrl.php
@@ -0,0 +1,33 @@
+<?php
+namespace LeoGalleguillos\Amazon\Model\Service\Product;
+
+use LeoGalleguillos\Amazon\Model\Entity as AmazonEntity;
+use LeoGalleguillos\Amazon\Model\Service as AmazonService;
+use LeoGalleguillos\String\Model\Service as StringService;
+
+class RootRelativeUrl
+{
+    public function __construct(
+        AmazonService\Product\ModifiedTitle $modifiedTitleService,
+        StringService\UrlFriendly $urlFriendlyService
+    ) {
+        $this->modifiedTitleService = $modifiedTitleService;
+        $this->urlFriendlyService   = $urlFriendlyService;
+    }
+
+    /**
+     * Get root-relative URL.
+     *
+     * @param AmazonEntity\Product $productEntity
+     * @return string
+     */
+    public function getRootRelativeUrl($productEntity) : string
+    {
+        $modifiedTitle = $this->modifiedTitleService->getModifiedTitle($productEntity);
+
+        return '/products/'
+             . (int) $productEntity->productId
+             . '/'
+             . $this->urlFriendlyService->getUrlFriendly($modifiedTitle);
+    }
+}
---
+++ test/Model/Service/Product/RootRelativeUrlTest.php
@@ -0,0 +1,46 @@
+<?php
+namespace LeoGalleguillos\AmazonTest\Model\Service;
+
+use LeoGalleguillos\Amazon\Model\Entity as AmazonEntity;
+use LeoGalleguillos\Amazon\Model\Service as AmazonService;
+use LeoGalleguillos\String\Model\Service as StringService;
+use PHPUnit\Framework\TestCase;
+
+class RootRelativeUrlTest extends TestCase
+{
+    protected function setUp()
+    {
+        $this->modifiedTitleServiceMock = $this->createMock(
+            AmazonService\Product\ModifiedTitle::class
+        );
+        $this->urlFriendlyServiceMock   = $this->createMock(
+            StringService\UrlFriendly::class
+        );
+        $this->rootRelativeUrlService = new AmazonService\Product\RootRelativeUrl(
+            $this->modifiedTitleServiceMock,
+            $this->urlFriendlyServiceMock
+        );
+    }
+
+    public function testInitialize()
+    {
+        $this->assertInstanceOf(
+            AmazonService\Product\RootRelativeUrl::class,
+            $this->rootRelativeUrlService
+        );
+    }
+
+    public function testGetModifiedTitle()
+    {
+        $productEntity            = new AmazonEntity\Product();
+        $productEntity->productId = 12345;
+        $productEntity->title     = 'My Amazing Product\'s Title (Is Great)';
+
+        $this->urlFriendlyServiceMock->method('getUrlFriendly')->willReturn('My-Product-Title');
+
+        $this->assertSame(
+            '/products/12345/My-Product-Title',
+            $this->rootRelativeUrlService->getRootRelativeUrl($productEntity)
+        );
+    }
+}
