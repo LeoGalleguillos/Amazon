cb2e615a15f270edd8f7f3ea4c70ebf285925e08
Leo Galleguillos <leo.galleguillos@icloud.com>
2018-01-11 11:37:36 -0800

    Toggle foreign key checks when dropping and creating test table

--- test/Model/Table/ProductTest.php
+++ test/Model/Table/ProductTest.php
@@ -13,7 +13,7 @@ class ProductTest extends TestCase
     /**
      * @var string
      */
-    protected $sqlPath = __DIR__ . '/../../..' . '/sql/leogalle_test/product/';
+    protected $sqlDirectory = __DIR__ . '/../../..' . '/sql/';
 
     protected function setUp()
     {
@@ -26,19 +26,41 @@ class ProductTest extends TestCase
             $this->adapter
         );
 
+        $this->setForeignKeyChecks0();
         $this->dropTable();
         $this->createTable();
+        $this->setForeignKeyChecks1();
+    }
+
+    protected function setForeignKeyChecks0()
+    {
+        $sql     = file_get_contents(
+            $this->sqlDirectory . 'SetForeignKeyChecks0.sql'
+        );
+        $result = $this->adapter->query($sql)->execute();
+    }
+
+    protected function setForeignKeyChecks1()
+    {
+        $sql     = file_get_contents(
+            $this->sqlDirectory . 'SetForeignKeyChecks1.sql'
+        );
+        $result = $this->adapter->query($sql)->execute();
     }
 
     protected function dropTable()
     {
-        $sql    = file_get_contents($this->sqlPath . 'drop.sql');
+        $sql    = file_get_contents(
+            $this->sqlDirectory . 'leogalle_test/product/drop.sql'
+        );
         $result = $this->adapter->query($sql)->execute();
     }
 
     protected function createTable()
     {
-        $sql    = file_get_contents($this->sqlPath . 'create.sql');
+        $sql    = file_get_contents(
+            $this->sqlDirectory . 'leogalle_test/product/create.sql'
+        );
         $result = $this->adapter->query($sql)->execute();
     }
 
