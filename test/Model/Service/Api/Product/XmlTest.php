607d4f2baf2b50eeafd513f5ded6bbe3a28f598e
Leo Galleguillos <leo.galleguillos@icloud.com>
2018-02-01 15:21:55 -0800

    Create service to get product XML

---
+++ test/Model/Service/Api/Product/XmlTest.php
@@ -0,0 +1,48 @@
+<?php
+namespace LeoGalleguillos\AmazonTest\Model\Service\Api\Product;
+
+use LeoGalleguillos\Amazon\Model\Service as AmazonService;
+use PHPUnit\Framework\TestCase;
+use SimpleXMLElement;
+
+class XmlTest extends TestCase
+{
+    protected function setUp()
+    {
+        $configArray = require($_SERVER['PWD'] . '/config/autoload/local.php');
+        $this->xmlService = new AmazonService\Api\Product\Xml(
+            $configArray['amazon']['access_key_id'],
+            $configArray['amazon']['associate_tag'],
+            $configArray['amazon']['secret_access_key']
+        );
+    }
+
+    public function testInitialize()
+    {
+        $this->assertInstanceOf(
+            AmazonService\Api\Product\Xml::class,
+            $this->xmlService
+        );
+    }
+
+    public function testGetXml()
+    {
+        $asin = 'B008FV5R2C';
+        $xml  = $this->xmlService->getXml($asin);
+
+        $this->assertInstanceOf(
+            SimpleXMLElement::class,
+            $xml
+        );
+
+        $this->assertInstanceOf(
+            SimpleXMLElement::class,
+            $xml->{'Items'}->{'Item'}
+        );
+
+        $this->assertSame(
+            $asin,
+            (string) $xml->{'Items'}->{'Item'}->{'ASIN'}
+        );
+    }
+}
