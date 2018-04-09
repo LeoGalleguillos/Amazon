bd51646da17e7b7648a7328d3cb17025c12bb2ea
Leo Galleguillos <leo.galleguillos@icloud.com>
2018-04-09 14:30:25 -0700

    Write test for getters and setters

---
+++ test/Model/Entity/ProductTest.php
@@ -0,0 +1,35 @@
+<?php
+namespace LeoGalleguillos\AmazonTest\Model\Entity;
+
+use LeoGalleguillos\Amazon\Model\Entity as AmazonEntity;
+use LeoGalleguillos\Image\Model\Entity as ImageEntity;
+use PHPUnit\Framework\TestCase;
+
+class ProductTest extends TestCase
+{
+    protected function setUp()
+    {
+        $this->productEntity = new AmazonEntity\Product();
+    }
+
+    public function testInitialize()
+    {
+        $this->assertInstanceOf(
+            AmazonEntity\Product::class,
+            $this->productEntity
+        );
+    }
+
+    public function testGettersAndSetters()
+    {
+        $imageEntity = new ImageEntity\Image();
+        $this->assertSame(
+            $this->productEntity,
+            $this->productEntity->setPrimaryImage($imageEntity)
+        );
+        $this->assertSame(
+            $imageEntity,
+            $this->productEntity->getPrimaryImage()
+        );
+    }
+}
