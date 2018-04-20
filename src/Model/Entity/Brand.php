09dda7bd6cdeacd33b9f1bff5a01055df3a5dedd
Leo Galleguillos <leo.galleguillos@icloud.com>
2018-04-20 03:11:22 +0000

    Add getters and setters to brand entity

--- src/Model/Entity/Brand.php
+++ src/Model/Entity/Brand.php
@@ -6,12 +6,17 @@ use LeoGalleguillos\Amazon\Model\Entity as AmazonEntity;
 class Brand
 {
     /**
-     * @var string Name of brand.
+     * @var int
+     */
+    protected $brandId;
+
+    /**
+     * @var string
      */
     public $name;
 
     /**
-     * @var string Slug of brand.
+     * @var string
      */
     public $slug;
 
@@ -36,9 +41,26 @@ class Brand
         return $this->name;
     }
 
+    public function getSlug() : string
+    {
+        return $this->slug;
+    }
+
+    public function setBrandId(int $brandId) : AmazonEntity\Brand
+    {
+        $this->brandId = $brandId;
+        return this;
+    }
+
     public function setName(string $name) : AmazonEntity\Brand
     {
         $this->name = $name;
+        return this;
+    }
+
+    public function setSlug(string $slug) : AmazonEntity\Brand
+    {
+        $this->slug = $slug;
         return this;
     }
 }
