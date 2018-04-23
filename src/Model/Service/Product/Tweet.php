2871e11b412feb0a67b3d9475e408a8f5d825e66
Leo Galleguillos <leo.galleguillos@icloud.com>
2018-04-23 10:17:27 +0000

    Add required shortcut

--- src/Model/Service/Product/Tweet.php
+++ src/Model/Service/Product/Tweet.php
@@ -1,6 +1,7 @@
 <?php
 namespace LeoGalleguillos\Amazon\Model\Service\Product;
 
+use LeoGalleguillos\Amazon\Model\Entity as AmazonEntity;
 use LeoGalleguillos\Amazon\Model\Factory as AmazonFactory;
 use LeoGalleguillos\Amazon\Model\Service as AmazonService;
 use LeoGalleguillos\Amazon\Model\Table as AmazonTable;
