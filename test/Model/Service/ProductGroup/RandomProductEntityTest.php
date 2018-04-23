b2901ae05fa2cd7a74f8c4528766e3b5e50fe588
Leo Galleguillos <leo.galleguillos@icloud.com>
2018-04-23 03:29:00 +0000

    Write preliminary test for random product entity service

---
+++ test/Model/Service/ProductGroup/RandomProductEntityTest.php
@@ -0,0 +1,33 @@
+<?php
+namespace LeoGalleguillos\AmazonTest\Model\Service;
+
+use LeoGalleguillos\Amazon\Model\Factory as AmazonFactory;
+use LeoGalleguillos\Amazon\Model\Service as AmazonService;
+use LeoGalleguillos\Amazon\Model\Table as AmazonTable;
+use PHPUnit\Framework\TestCase;
+
+class RandomProductEntityTest extends TestCase
+{
+    protected function setUp()
+    {
+        $this->productFactoryMock = $this->createMock(
+            AmazonFactory\Product::class
+        );
+        $this->productIdTableMock = $this->createMock(
+            AmazonTable\Product\ProductId::class
+        );
+
+        $this->randomProductEntityService = new AmazonService\ProductGroup\RandomProductEntity(
+            $this->productFactoryMock,
+            $this->productIdTableMock
+        );
+    }
+
+    public function testInitialize()
+    {
+        $this->assertInstanceOf(
+            AmazonService\ProductGroup\RandomProductEntity::class,
+            $this->randomProductEntityService
+        );
+    }
+}
