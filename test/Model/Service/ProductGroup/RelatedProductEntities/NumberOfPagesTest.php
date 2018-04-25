ac82bee789433ba1c62fcd4d802c0d900a4d6885
Leo Galleguillos <leo.galleguillos@icloud.com>
2018-04-25 02:06:42 +0000

    Write preliminary number of pages service

---
+++ test/Model/Service/ProductGroup/RelatedProductEntities/NumberOfPagesTest.php
@@ -0,0 +1,33 @@
+<?php
+namespace LeoGalleguillos\AmazonTest\Model\Service\ProductGroup\RelatedProductEntities;
+
+use LeoGalleguillos\Amazon\Model\Factory as AmazonFactory;
+use LeoGalleguillos\Amazon\Model\Service as AmazonService;
+use LeoGalleguillos\Amazon\Model\Table as AmazonTable;
+use PHPUnit\Framework\TestCase;
+
+class NumberOfPagesTest extends TestCase
+{
+    protected function setUp()
+    {
+        $this->modifiedTitleServiceMock = $this->createMock(
+            AmazonService\Product\ModifiedTitle::class
+        );
+        $this->productGroupTableMock = $this->createMock(
+            AmazonTable\Search\ProductGroup::class
+        );
+
+        $this->numberOfPagesService = new AmazonService\ProductGroup\RelatedProductEntities\NumberOfPages(
+            $this->modifiedTitleServiceMock,
+            $this->productGroupTableMock
+        );
+    }
+
+    public function testInitialize()
+    {
+        $this->assertInstanceOf(
+            AmazonService\ProductGroup\RelatedProductEntities\NumberOfPages::class,
+            $this->numberOfPagesService
+        );
+    }
+}
