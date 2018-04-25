2727774a4fc3e74934699b49c152d02dbcc6badd
Leo Galleguillos <leo.galleguillos@icloud.com>
2018-04-25 01:27:46 +0000

    Write preliminary test for getting related product entities

---
+++ test/Model/Service/ProductGroup/RelatedProductEntitiesTest.php
@@ -0,0 +1,37 @@
+<?php
+namespace LeoGalleguillos\AmazonTest\Model\Service\ProductGroup;
+
+use LeoGalleguillos\Amazon\Model\Factory as AmazonFactory;
+use LeoGalleguillos\Amazon\Model\Service as AmazonService;
+use LeoGalleguillos\Amazon\Model\Table as AmazonTable;
+use PHPUnit\Framework\TestCase;
+
+class RelatedProductEntitiesTest extends TestCase
+{
+    protected function setUp()
+    {
+        $this->productFactoryMock = $this->createMock(
+            AmazonFactory\Product::class
+        );
+        $this->modifiedTitleServiceMock = $this->createMock(
+            AmazonService\Product\ModifiedTitle::class
+        );
+        $this->productGroupTableMock = $this->createMock(
+            AmazonTable\Search\ProductGroup::class
+        );
+
+        $this->relatedProductEntitiesService = new AmazonService\ProductGroup\RelatedProductEntities(
+            $this->productFactoryMock,
+            $this->modifiedTitleServiceMock,
+            $this->productGroupTableMock
+        );
+    }
+
+    public function testInitialize()
+    {
+        $this->assertInstanceOf(
+            AmazonService\ProductGroup\RelatedProductEntities::class,
+            $this->relatedProductEntitiesService
+        );
+    }
+}
