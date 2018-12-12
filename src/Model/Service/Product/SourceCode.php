904981b3bb917cb50e781e37d28a57c1ef7573f1
Leo Galleguillos <leo.galleguillos@icloud.com>
2018-12-12 13:55:44 +0000

    Update source code service to use curl instead of file_get_contents

--- src/Model/Service/Product/SourceCode.php
+++ src/Model/Service/Product/SourceCode.php
@@ -7,8 +7,17 @@ class SourceCode
 {
     public function getSourceCode(AmazonEntity\Product $productEntity): string
     {
-        return file_get_contents(
-            'https://www.amazon.com/dp/' . $productEntity->getAsin()
-        );
+        $url = 'https://www.amazon.com/dp/' . $productEntity->getAsin();
+
+        $ch = curl_init($url);
+        $curlOptions = [
+            CURLOPT_ENCODING       => 'gzip',
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_USERAGENT      => 'Mozilla/5.0 (Windows NT 6.1; rv:8.0) Gecko/20100101 Firefox/8.0',
+        ];
+
+        curl_setopt_array($ch, $curlOptions);
+
+        return curl_exec($ch);
     }
 }
