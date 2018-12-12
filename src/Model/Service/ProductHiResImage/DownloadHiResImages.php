dffefc9e98fc1c1eafa8c0a5475ace821d35391b
Leo Galleguillos <leo.galleguillos@icloud.com>
2018-12-12 03:55:13 +0000

    Do not throw exception if hi res image was already downloaded

--- src/Model/Service/ProductHiResImage/DownloadHiResImages.php
+++ src/Model/Service/ProductHiResImage/DownloadHiResImages.php
@@ -14,17 +14,20 @@ class DownloadHiResImages
         if (preg_match('/\W/', $asin)) {
             throw new Exception('ASIN contains invalid characters');
         }
-        mkdir('/home/amazon/products/hi-res-images/' . $asin);
+
+        if (!file_exists('/home/amazon/products/hi-res-images/')) {
+            mkdir('/home/amazon/products/hi-res-images/' . $asin);
+        }
 
         foreach ($hiResImages as $hiResImage) {
             $fileName = basename($hiResImage->getUrl());
-            if (!preg_match('/^[\w\.]+$/', $fileName)) {
-                throw new Exception('File name contains invalid characters');
+            if (!preg_match('/^[\w\.\_\%\-]+$/', $fileName)) {
+                throw new Exception('Invalid file name (this should never happen)');
             }
             $destination = "/home/amazon/products/hi-res-images/$asin/$fileName";
 
             if (file_exists($destination)) {
-                throw new Exception('File already exists');
+                continue;
             }
 
             copy(
