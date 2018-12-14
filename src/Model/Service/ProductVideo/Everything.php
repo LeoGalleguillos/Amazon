8741a41307d13ac966b1821305b0b8c63b19bf10
Leo Galleguillos <leo.galleguillos@icloud.com>
2018-12-14 06:13:19 +0000

    Add missing shortcut

--- src/Model/Service/ProductVideo/Everything.php
+++ src/Model/Service/ProductVideo/Everything.php
@@ -5,6 +5,7 @@ use LeoGalleguillos\Amazon\Model\Entity as AmazonEntity;
 use LeoGalleguillos\Amazon\Model\Factory as AmazonFactory;
 use LeoGalleguillos\Amazon\Model\Service as AmazonService;
 use LeoGalleguillos\Amazon\Model\Table as AmazonTable;
+use TypeError;
 
 /**
  * Everything
