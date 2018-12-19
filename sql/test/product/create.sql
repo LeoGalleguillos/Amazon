0c9e27c786ad1d5fa434e357893553262583d055
Leo Galleguillos <leo.galleguillos@icloud.com>
2018-12-19 03:27:23 +0000

    Fix key name

--- sql/test/product/create.sql
+++ sql/test/product/create.sql
@@ -23,5 +23,5 @@ CREATE TABLE `product` (
   KEY `product_group_similar_retrieved` (`product_group`,`similar_retrieved`),
   KEY `hi_res_images_retrieved_video_generated_created` (`hi_res_images_retrieved`,`video_generated`, `created`),
   KEY `similar_retrieved_created` (`similar_retrieved`, `created`),
-  KEY `similar_retrieved_created` (`video_generated`, `created`)
+  KEY `video_generated_created` (`video_generated`, `created`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8
