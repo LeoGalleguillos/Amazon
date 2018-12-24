c0e2b917bc88e1539233c9847a4ca61ace6fcffe
Leo Galleguillos <leo.galleguillos@icloud.com>
2018-12-24 08:07:02 +0000

    Start to use TableTestCase

--- test/Model/Table/ProductTest.php
+++ test/Model/Table/ProductTest.php
@@ -4,49 +4,25 @@ namespace LeoGalleguillos\AmazonTest\Model\Table;
 use ArrayObject;
 use LeoGalleguillos\Amazon\Model\Entity as AmazonEntity;
 use LeoGalleguillos\Amazon\Model\Table as AmazonTable;
-use LeoGalleguillos\AmazonTest as AmazonTest;
 use LeoGalleguillos\Memcached\Model\Service as MemcachedService;
+use LeoGalleguillos\Test\TableTestCase;
 use Zend\Db\Adapter\Adapter;
 use PHPUnit\Framework\TestCase;
 
-class ProductTest extends AmazonTest\TableCase
+class ProductTest extends TableTestCase
 {
-    /**
-     * @var string
-     */
-    protected $sqlDirectory = __DIR__ . '/../../..' . '/sql/';
-
     protected function setUp()
     {
         $this->memcachedService = $this->createMock(MemcachedService\Memcached::class);
-        $configArray            = require(__DIR__ . '/../../../config/autoload/local.php');
-        $configArray            = $configArray['db']['adapters']['leogalle_test'];
-        $this->adapter          = new Adapter($configArray);
         $this->productTable     = new AmazonTable\Product(
             $this->memcachedService,
-            $this->adapter
+            $this->getAdapter()
         );
 
         $this->setForeignKeyChecks0();
-        $this->dropTable();
-        $this->createTable();
+        $this->dropTable('product');
+        $this->createTable('product');
         $this->setForeignKeyChecks1();
-    }
-
-    protected function dropTable()
-    {
-        $sql    = file_get_contents(
-            $this->sqlDirectory . 'leogalle_test/product/drop.sql'
-        );
-        $result = $this->adapter->query($sql)->execute();
-    }
-
-    protected function createTable()
-    {
-        $sql    = file_get_contents(
-            $this->sqlDirectory . 'leogalle_test/product/create.sql'
-        );
-        $result = $this->adapter->query($sql)->execute();
     }
 
     public function testInitialize()
