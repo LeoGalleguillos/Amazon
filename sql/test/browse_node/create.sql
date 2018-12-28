58050d6f3d6746697180859dff05b2cdf5c788fc
Leo Galleguillos <leo.galleguillos@icloud.com>
2018-12-28 06:29:38 +0000

    Browse node ID value can exceed 4294967295

--- sql/test/browse_node/create.sql
+++ sql/test/browse_node/create.sql
@@ -1,5 +1,5 @@
 CREATE TABLE `browse_node` (
-  `browse_node_id` int(10) unsigned NOT NULL,
+  `browse_node_id` BIGINT(10) unsigned NOT NULL,
   `name` varchar(255) NOT NULL,
   PRIMARY KEY (`browse_node_id`)
 ) ENGINE=InnoDB CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
--- sql/test/browse_node_product/create.sql
+++ sql/test/browse_node_product/create.sql
@@ -1,5 +1,5 @@
 CREATE TABLE `browse_node_product` (
-  `browse_node_id` int(10) unsigned NOT NULL,
+  `browse_node_id` BIGINT(10) unsigned NOT NULL,
   `product_id` int(10) unsigned NOT NULL,
   PRIMARY KEY (`browse_node_id`, `product_id`)
 ) ENGINE=InnoDB CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
