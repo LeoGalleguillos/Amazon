eb1c6a39bf82e89cbd401e008c83dc603140f528
Leo Galleguillos <leo.galleguillos@icloud.com>
2018-12-30 05:22:53 +0000

    Add index on browse_node_product.product_id column

--- sql/test/browse_node_product/create.sql
+++ sql/test/browse_node_product/create.sql
@@ -1,6 +1,7 @@
 CREATE TABLE `browse_node_product` (
   `browse_node_id` BIGINT(10) unsigned NOT NULL,
   `product_id` int(10) unsigned NOT NULL,
-  PRIMARY KEY (`browse_node_id`, `product_id`)
+  PRIMARY KEY (`browse_node_id`, `product_id`),
+  KEY `product_id` (`product_id`)
 ) ENGINE=InnoDB CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
