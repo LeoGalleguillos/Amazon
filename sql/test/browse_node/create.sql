1a8d6fa651a615bd48ec23ee5e730edf6778f815
Leo Galleguillos <leo.galleguillos@icloud.com>
2019-01-08 11:17:08 +0000

    Add index on browse_node.name column

--- sql/test/browse_node/create.sql
+++ sql/test/browse_node/create.sql
@@ -1,6 +1,7 @@
 CREATE TABLE `browse_node` (
   `browse_node_id` BIGINT(10) unsigned NOT NULL,
   `name` varchar(255) NOT NULL,
-  PRIMARY KEY (`browse_node_id`)
+  PRIMARY KEY (`browse_node_id`),
+  INDEX `name` (`name`)
 ) ENGINE=InnoDB CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
