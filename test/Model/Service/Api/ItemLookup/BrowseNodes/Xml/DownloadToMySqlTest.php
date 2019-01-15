723a7bde45a3df6ec1a296edd32fa803b737c6d6
Leo Galleguillos <leo.galleguillos@icloud.com>
2019-01-15 10:48:29 +0000

    Write preliminary test for browse nodes download to mysql service

---
+++ test/Model/Service/Api/ItemLookup/BrowseNodes/Xml/DownloadToMySqlTest.php
@@ -0,0 +1,36 @@
+<?php
+namespace LeoGalleguillos\AmazonTest\Model\Service\Api\ItemLookup\BrowseNodes;
+
+use LeoGalleguillos\Amazon\Model\Service as AmazonService;
+use LeoGalleguillos\Amazon\Model\Table as AmazonTable;
+use PHPUnit\Framework\TestCase;
+use SimpleXMLElement;
+
+class DownloadToMySqlTest extends TestCase
+{
+    protected function setUp()
+    {
+        $this->browseNodeTableMock = $this->createMock(
+            AmazonTable\BrowseNode::class
+        );
+        $this->browseNodeProductTableMock = $this->createMock(
+            AmazonTable\BrowseNodeProduct::class
+        );
+        $this->asinTableMock = $this->createMock(
+            AmazonTable\Product\Asin::class
+        );
+        $this->downloadToMySqlService = new AmazonService\Api\ItemLookup\BrowseNodes\Xml\DownloadToMySql(
+            $this->browseNodeTableMock,
+            $this->browseNodeProductTableMock,
+            $this->asinTableMock
+        );
+    }
+
+    public function testInitialize()
+    {
+        $this->assertInstanceOf(
+            AmazonService\Api\ItemLookup\BrowseNodes\Xml\DownloadToMySql::class,
+            $this->downloadToMySqlService
+        );
+    }
+}
