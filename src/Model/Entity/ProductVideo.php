dd6e696fb22837b7bd47feb2b86d784568476628
Leo Galleguillos <leo.galleguillos@icloud.com>
2019-01-27 00:47:29 +0000

    Add product video ID property to product video entity class

--- src/Model/Entity/ProductVideo.php
+++ src/Model/Entity/ProductVideo.php
@@ -11,14 +11,30 @@ class ProductVideo extends VideoEntity\Video
      */
     protected $product;
 
+    /**
+     * @var int
+     */
+    protected $productVideoId;
+
     public function getProduct(): AmazonEntity\Product
     {
         return $this->product;
     }
 
+    public function getProductVideoId(): int
+    {
+        return $this->productVideoId;
+    }
+
     public function setProduct(AmazonEntity\Product $product): AmazonEntity\ProductVideo
     {
         $this->product = $product;
+        return $this;
+    }
+
+    public function setProductVideoId(int $productVideoId): AmazonEntity\ProductVideo
+    {
+        $this->productVideoId = $productVideoId;
         return $this;
     }
 }
