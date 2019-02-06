e0ab8399d7ab21f37c3f721a9c8f880f11704e9c
Leo Galleguillos <leo.galleguillos@icloud.com>
2019-02-06 21:39:38 +0000

    Add key on url column

--- sql/test/product_image/create.sql
+++ sql/test/product_image/create.sql
@@ -4,5 +4,6 @@ CREATE TABLE `product_image` (
   `url` varchar(128) NOT NULL,
   `width` int(11) DEFAULT NULL,
   `height` int(11) DEFAULT NULL,
-  UNIQUE KEY `asin_category_url` (`asin`,`category`,`url`)
+  UNIQUE KEY `asin_category_url` (`asin`,`category`,`url`),
+  KEY `url` (`url`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
