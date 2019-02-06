c637936091eb0bbd3c581a96f1f5378a9f890086
Leo Galleguillos <leo.galleguillos@icloud.com>
2019-02-06 21:37:08 +0000

    Write sql files for product_image table

---
+++ sql/test/product_image/create.sql
@@ -0,0 +1,8 @@
+CREATE TABLE `product_image` (
+  `asin` varchar(10) NOT NULL,
+  `category` varchar(255) NOT NULL,
+  `url` varchar(128) NOT NULL,
+  `width` int(11) DEFAULT NULL,
+  `height` int(11) DEFAULT NULL,
+  UNIQUE KEY `asin_category_url` (`asin`,`category`,`url`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
