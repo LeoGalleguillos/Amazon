0e192272a91986cd613eb4941f1516415d54cfdf
Leo Galleguillos <leo.galleguillos@icloud.com>
2019-02-11 08:43:25 +0000

    Write view helper to get browse node breadcrumbs html

---
+++ src/View/Helper/BrowseNode/BreadcrumbsHtml.php
@@ -0,0 +1,48 @@
+<?php
+namespace LeoGalleguillos\Amazon\View\Helper\BrowseNode;
+
+use LeoGalleguillos\Amazon\Model\Entity as AmazonEntity;
+use LeoGalleguillos\Amazon\Model\Service as AmazonService;
+use LeoGalleguillos\String\Model\Service as StringService;
+use Zend\View\Helper\AbstractHelper;
+
+class BreadcrumbsHtml extends AbstractHelper
+{
+    public function __construct(
+        AmazonService\BrowseNode\BrowseNodes\Breadcrumbs $breadcrumbsService,
+        StringService\Escape $escapeService,
+        StringService\UrlFriendly $urlFriendlyService
+    ) {
+        $this->breadcrumbsService = $breadcrumbsService;
+        $this->escapeService      = $escapeService;
+        $this->urlFriendlyService = $urlFriendlyService;
+    }
+
+    public function __invoke(AmazonEntity\BrowseNode $browseNodeEntity): string
+    {
+        $browseNodeEntities = $this->breadcrumbsService->getBrowseNodes(
+            $browseNodeEntity
+        );
+
+        $href = '/categories';
+        $html = "<ol class=\"breadcrumbs\">\n";
+
+        foreach ($browseNodeEntities as $browseNodeEntity) {
+            $nameUrlFriendly = $this->urlFriendlyService->getUrlFriendly(
+                $browseNodeEntity->getName()
+            );
+            $href .= '/'
+                . $browseNodeEntity->getBrowseNodeId()
+                . '/'
+                . $nameUrlFriendly;
+            $nameEscaped = $this->escapeService->escape(
+                $browseNodeEntity->getName()
+            );
+            $html .= "<li><a href=\"$href\">$nameEscaped</a></li>\n";
+        }
+
+        $html .= '</ol>';
+
+        return $html;
+    }
+}
