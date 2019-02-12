5b968f3c044053f9559ac15b33f0aaabdde4812a
Leo Galleguillos <leo.galleguillos@icloud.com>
2019-02-12 00:43:01 +0000

    Index on name, browse_node_id columns can be unique

--- sql/test/browse_node/create.sql
+++ sql/test/browse_node/create.sql
@@ -2,6 +2,6 @@ CREATE TABLE `browse_node` (
   `browse_node_id` BIGINT(10) unsigned NOT NULL,
   `name` varchar(255) NOT NULL,
   PRIMARY KEY (`browse_node_id`),
-  INDEX `name_browse_node_id` (`name`, `browse_node_id`)
+  UNIQUE `name_browse_node_id` (`name`, `browse_node_id`)
 ) ENGINE=InnoDB CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
