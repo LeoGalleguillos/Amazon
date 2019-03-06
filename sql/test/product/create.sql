e8f6047bc81f384de013355f4df8d3568125b297
Leo Galleguillos <leo.galleguillos@icloud.com>
2019-03-06 02:55:29 +0000

    Remove hashtags_retrieved column from product table

--- sql/test/product/create.sql
+++ sql/test/product/create.sql
@@ -8,7 +8,6 @@ CREATE TABLE `product` (
   `list_price` float(16,2) DEFAULT NULL,
   `created` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `modified` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `hashtags_retrieved` datetime DEFAULT NULL,
   `hi_res_images_retrieved` datetime DEFAULT NULL,
   `similar_retrieved` datetime DEFAULT NULL,
   `video_generated` datetime DEFAULT NULL,
