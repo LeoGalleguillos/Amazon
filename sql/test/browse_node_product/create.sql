24821c15dfeae5472b181c976c436d766ae80f1e
Leo Galleguillos <leo.galleguillos@icloud.com>
2019-04-19 18:59:48 +0000

    Add order tinyint(2) column to browse_node_product table

--- sql/test/browse_node_product/create.sql
+++ sql/test/browse_node_product/create.sql
@@ -1,6 +1,7 @@
 CREATE TABLE `browse_node_product` (
-  `browse_node_id` BIGINT(10) unsigned NOT NULL,
+  `browse_node_id` bigint(10) unsigned NOT NULL,
   `product_id` int(10) unsigned NOT NULL,
+  `order` TINYINT(2) unsigned DEFAULT NULL,
   PRIMARY KEY (`browse_node_id`, `product_id`),
   UNIQUE `product_id_browse_node_id` (`product_id`, `browse_node_id`)
 ) ENGINE=InnoDB CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
