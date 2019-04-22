5cda44ecdcc266866cfa9c84810f854b7d919251
Leo Galleguillos <leo.galleguillos@icloud.com>
2019-04-22 17:13:44 +0000

    Update unique index to contain `order` instead of `browse_node_id`

--- sql/test/browse_node_product/create.sql
+++ sql/test/browse_node_product/create.sql
@@ -3,6 +3,6 @@ CREATE TABLE `browse_node_product` (
   `product_id` int(10) unsigned NOT NULL,
   `order` TINYINT(2) unsigned DEFAULT NULL,
   PRIMARY KEY (`browse_node_id`, `product_id`),
-  UNIQUE `product_id_browse_node_id` (`product_id`, `browse_node_id`)
+  UNIQUE `product_id_order` (`product_id`, `order`)
 ) ENGINE=InnoDB CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
