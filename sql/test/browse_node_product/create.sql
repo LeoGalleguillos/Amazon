13829bb79d63d2afccad7d06b87d22e5bc4f3eff
Leo Galleguillos <leo.galleguillos@icloud.com>
2019-05-02 19:48:29 +0000

    `browse_node_product`.`order` column cannot be null

--- sql/test/browse_node_product/create.sql
+++ sql/test/browse_node_product/create.sql
@@ -1,7 +1,7 @@
 CREATE TABLE `browse_node_product` (
   `browse_node_id` bigint(10) unsigned NOT NULL,
   `product_id` int(10) unsigned NOT NULL,
-  `order` TINYINT(2) unsigned DEFAULT NULL,
+  `order` TINYINT(2) unsigned NOT NULL,
   PRIMARY KEY (`browse_node_id`, `product_id`),
   UNIQUE `product_id_order` (`product_id`, `order`)
 ) ENGINE=InnoDB CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
