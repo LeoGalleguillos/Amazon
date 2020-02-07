1a5ed025465d028cbd3bf69e250b049f5f6a5000
Leo Galleguillos <leo.galleguillos@icloud.com>
2020-02-07 06:55:39 +0000

    Write unit test for ProductTableGateway

---
+++ test/Model/TableGateway/ProductTest.php
@@ -0,0 +1,46 @@
+<?php
+namespace LeoGalleguillos\AmazonTest\Model\TableGateway;
+
+use LeoGalleguillos\Amazon\Model\TableGateway as AmazonTableGateway;
+use LeoGalleguillos\Test\TableTestCase;
+
+class ProductTest extends TableTestCase
+{
+    protected function setUp()
+    {
+        $this->dropTable('product');
+        $this->createTable('product');
+
+        $this->productTableGateway = new AmazonTableGateway\Product(
+            'product',
+            $this->getAdapter()
+        );
+    }
+
+    public function testInsertAndUpdate()
+    {
+        $affectedRows = $this->productTableGateway->update(
+            [
+                'color' => 'BLACK',
+            ],
+            ['product_id' => 12345]
+        );
+        $this->assertSame(0, $affectedRows);
+        $affectedRows = $this->productTableGateway->insert(
+            [
+                'asin' => 'ASIN',
+                'title' => 'Title',
+                'product_group' => 'Product Group',
+                'color' => 'BLACK',
+            ]
+        );
+        $this->assertSame(1, $affectedRows);
+        $affectedRows = $this->productTableGateway->update(
+            [
+                'color' => 'RED',
+            ],
+            ['product_id' => 1]
+        );
+        $this->assertSame(1, $affectedRows);
+    }
+}
