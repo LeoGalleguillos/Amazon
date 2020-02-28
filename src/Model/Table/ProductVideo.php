0b42e6347315905b4a53b678b984bd9947380455
Leo Galleguillos <leo.galleguillos@icloud.com>
2020-02-28 06:22:24 +0000

    Add unique column to order by clause

--- src/Model/Table/ProductVideo.php
+++ src/Model/Table/ProductVideo.php
@@ -316,6 +316,7 @@ class ProductVideo
 
              ORDER
                 BY `product_video`.`created` DESC
+                 , `product_video`.`product_video_id` ASC
 
              LIMIT ?, ?
                  ;
