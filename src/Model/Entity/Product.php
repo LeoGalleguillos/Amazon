412fc9e5921071d73cdbe21ad4ac2746d9784d6e
Leo Galleguillos <leo.galleguillos@icloud.com>
2020-03-01 08:24:36 +0000

    Improve @deprecated docblock

--- src/Model/Entity/Product.php
+++ src/Model/Entity/Product.php
@@ -39,7 +39,7 @@ class Product
 
     /**
      * @var array
-     * @deprecated Use $this->browseNodeProducts property instead
+     * @deprecated Use ::browseNodeProducts instead
      */
     protected $browseNodes;
 
