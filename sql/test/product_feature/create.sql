44ee1aafcf00d62d27b1d66fce42cac6973dfda8
Leo Galleguillos <leo.galleguillos@icloud.com>
2020-03-05 06:54:57 +0000

    Write SQL create and drop statements for `product_feature` table

---
+++ sql/test/product_feature/create.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `product_feature` (
+  `product_id` int(10) unsigned DEFAULT NULL,
+  `asin` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `feature` mediumtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  KEY `asin` (`asin`),
+  KEY `product_id` (`product_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
