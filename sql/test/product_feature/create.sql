bd5e5a970f189124284520bb569cc31d41ed24f9
Leo Galleguillos <leo.galleguillos@icloud.com>
2020-03-05 17:08:17 +0000

    Add foreign key constraint on `product_feature`.`product_id` which references `product`.`product_id`

--- sql/test/product_feature/create.sql
+++ sql/test/product_feature/create.sql
@@ -3,5 +3,9 @@ CREATE TABLE `product_feature` (
   `asin` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL,
   `feature` mediumtext COLLATE utf8mb4_unicode_ci NOT NULL,
   KEY `asin` (`asin`),
-  KEY `product_id` (`product_id`)
+  KEY `product_id` (`product_id`),
+  CONSTRAINT `product_product_feature` FOREIGN KEY `product_id` (`product_id`)
+    REFERENCES `product` (`product_id`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
