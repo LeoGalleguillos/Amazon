73f8c936fd9051ecf7ed31281a738b63141cfddc
Leo Galleguillos <leo.galleguillos@icloud.com>
2020-03-06 08:14:31 +0000

    Add foreign key contraints on `product_similar`.`asin` and `product_similar`.`similar_asin` columns

--- sql/test/product_similar/create.sql
+++ sql/test/product_similar/create.sql
@@ -2,5 +2,13 @@ CREATE TABLE `product_similar` (
   `asin` varchar(10) NOT NULL, 
   `similar_asin` varchar(10) NOT NULL, 
   UNIQUE KEY `asin` (`asin`,`similar_asin`),
-  KEY `similar_asin` (`similar_asin`)     
+  KEY `similar_asin` (`similar_asin`),
+  CONSTRAINT `product_product_similar_asin` FOREIGN KEY (`asin`)
+    REFERENCES `product` (`asin`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
+  CONSTRAINT `product_product_similar_similar_asin` FOREIGN KEY (`similar_asin`)
+    REFERENCES `product` (`asin`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
 ) ENGINE=InnoDB CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
