372bcc8f8ff34e851035552e1965740cf08b3bf6
Leo Galleguillos <leo.galleguillos@icloud.com>
2020-03-06 06:29:50 +0000

    Write preliminiary test for ProductImage table model

---
+++ test/Model/Table/ProductImageTest.php
@@ -0,0 +1,32 @@
+<?php
+namespace LeoGalleguillos\AmazonTest\Model\Table;
+
+use LeoGalleguillos\Amazon\Model\Table as AmazonTable;
+use LeoGalleguillos\Memcached\Model\Service as MemcachedService;
+use LeoGalleguillos\Test\TableTestCase;
+
+class ProductImageTest extends TableTestCase
+{
+    protected function setUp()
+    {
+        $this->setForeignKeyChecks(0);
+        $this->dropAndCreateTables(['product', 'product_image']);
+        $this->setForeignKeyChecks(1);
+
+        $this->productTable = new AmazonTable\Product(
+            $this->createMock(MemcachedService\Memcached::class),
+            $this->getAdapter()
+        );
+        $this->productImageTable = new AmazonTable\ProductImage(
+            $this->getAdapter()
+        );
+    }
+
+    public function testSelectWhereAsin()
+    {
+        $generator = $this->productImageTable->selectWhereAsin('ASIN009');
+        $this->assertEmpty(
+            iterator_to_array($generator)
+        );
+    }
+}
