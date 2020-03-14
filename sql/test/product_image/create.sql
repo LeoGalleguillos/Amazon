54d753812db2032d8795edfc6336777ec5b70354
Leo Galleguillos <leo.galleguillos@icloud.com>
2020-03-14 14:58:18 +0000

    Improve spacing

--- sql/test/product_image/create.sql
+++ sql/test/product_image/create.sql
@@ -6,8 +6,8 @@ CREATE TABLE `product_image` (
   `width` int(11) DEFAULT NULL,
   `height` int(11) DEFAULT NULL,
   KEY `product_id` (`product_id`),
-  UNIQUE KEY `asin_category_url` (`asin`,`category`,`url`),
-  UNIQUE KEY `product_id_category_url` (`product_id`,`category`,`url`),
+  UNIQUE KEY `asin_category_url` (`asin`, `category`, `url`),
+  UNIQUE KEY `product_id_category_url` (`product_id`, `category`, `url`),
   KEY `url` (`url`),
   CONSTRAINT `product_product_image` FOREIGN KEY `product_id` (`product_id`)
     REFERENCES `product` (`product_id`)
