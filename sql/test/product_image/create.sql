0b0e35979c2388f5fe0b65aef20b026b2edc9317
Leo Galleguillos <leo.galleguillos@icloud.com>
2020-03-14 15:04:32 +0000

    Remove `product_image`.`asin` column

--- sql/test/product_image/create.sql
+++ sql/test/product_image/create.sql
@@ -1,12 +1,10 @@
 CREATE TABLE `product_image` (
   `product_id` int(10) unsigned NOT NULL,
-  `asin` varchar(10) NOT NULL,
   `category` varchar(255) NOT NULL,
   `url` varchar(128) NOT NULL,
   `width` int(11) DEFAULT NULL,
   `height` int(11) DEFAULT NULL,
   KEY `product_id` (`product_id`),
-  UNIQUE KEY `asin_category_url` (`asin`, `category`, `url`),
   UNIQUE KEY `product_id_category_url` (`product_id`, `category`, `url`),
   KEY `url` (`url`),
   CONSTRAINT `product_product_image` FOREIGN KEY `product_id` (`product_id`)
